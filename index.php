ae84f3ec9506c5c6bf3d41c7cdf6041b06616c73
GabrielLucasGirardi <ggirardi111@gmail.com>
2022-09-01 13:29:58 -0300

    funcoes

--- index.php
+++ index.php
@@ -4,26 +4,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Viagens</title>
+    <script src="funcoes.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
     <h1>Calcular Gastos</h1>
-    <label for="nome_M" class="form-label">Nome do Motorista</label>
-    <input type="text" id="nome_M" class="form-control" name="NomeCompanhia" placeholder="Nome do Motorista"><br>
-    <label for="nome_s" class="form-label">Modelo do Carro</label>
-    <input type="text" id="nome_s" class="form-control" name="NomeCompanhia" placeholder="Modelo do Carro"><br>
-    <label for="nome_s" class="form-label">Placa do Carro</label>
-    <input type="text" id="nome_s" class="form-control" name="NomeCompanhia" placeholder="Placa do Carro"><br>
-    <label for="nome_s" class="form-label">Local de Origem</label>
-    <input type="text" id="nome_s" class="form-control" name="NomeCompanhia" placeholder="Local de Origem"><br>
-    <label for="nome_s" class="form-label">Local do Destino</label>
-    <input type="text" id="nome_s" class="form-control" name="NomeCompanhia" placeholder="Local do Destino"><br>
-    <label for="nome_s" class="form-label">Gasolina Gasta (Litros)</label>
-    <input type="text" id="nome_s" class="form-control" name="NomeCompanhia" placeholder="Litros"><br>
-    <label for="nome_s" class="form-label">Preço da Gasolina atual</label>
-    <input type="text" id="nome_s" class="form-control" name="NomeCompanhia" placeholder="Preço"><br>
-    <label for="nome_s" class="form-label">Distância Percorrida (KM)</label>
-    <input type="text" id="nome_s" class="form-control" name="NomeCompanhia" placeholder="Distância"><br>
+    <label for="NomeMotorista" class="form-label">Nome do Motorista</label>
+    <input type="text" id="NomeMotorista" class="form-control" name="NomeCompanhia" placeholder="Nome do Motorista"><br>
+    <label for="Modelo" class="form-label">Modelo do Carro</label>
+    <input type="text" id="Modelo" class="form-control" name="NomeCompanhia" placeholder="Modelo do Carro"><br>
+    <label for="Placa" class="form-label">Placa do Carro</label>
+    <input type="text" id="Placa" class="form-control" name="NomeCompanhia" placeholder="Placa do Carro"><br>
+    <label for="Origem" class="form-label">Local de Origem</label>
+    <input type="text" id="Origem" class="form-control" name="NomeCompanhia" placeholder="Local de Origem"><br>
+    <label for="Destino" class="form-label">Local do Destino</label>
+    <input type="text" id="Destino" class="form-control" name="NomeCompanhia" placeholder="Local do Destino"><br>
+    <label for="GasolinaGasta" class="form-label">Gasolina Gasta (Litros)</label>
+    <input type="text" id="GasolinaGasta" class="form-control" name="NomeCompanhia" placeholder="Litros"><br>
+    <label for="Preco" class="form-label">Preço da Gasolina atual</label>
+    <input type="text" id="Preco" class="form-control" name="NomeCompanhia" placeholder="Preço"><br>
+    <label for="Distancia" class="form-label">Distância Percorrida (KM)</label>
+    <input type="text" id="Distancia" class="form-control" name="NomeCompanhia" placeholder="Distância"><br>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
